f8d8c726931b25a154e4416bd254e614025fe8a3
rodri <rodrigo.porto1403@gmail.com>
2023-04-27 21:12:28 -0300

    chat component added

--- frontend/src/components/chat/index.tsx
+++ frontend/src/components/chat/index.tsx
@@ -1,14 +1,83 @@
+import React, { useState } from 'react';
 import './styles.css';
 
-const Chat = () =>
-{
-    return(
+class Message {
+    text: string;
+    isUser: boolean;
 
-        <section className="chat-container">
-            
-        </section>
-
-    )
+    constructor(text: string, isUser: boolean) {
+        this.text = text;
+        this.isUser = isUser;
+    }
 }
 
-export default Chat
+const Chat = () => {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        console.log()
+        setMessage(event.target.value);
+    };
+
+    const handleSendMessage = () => {
+        const trimmedMessage = message.trim();
+        if (trimmedMessage) {
+            const newMessage = new Message(trimmedMessage, true);
+
+            fetch('https://api.chucknorris.io/jokes/random')
+                .then(response => response.json())
+                .then(data => {
+                    const botMessage = new Message(data.value, false);
+                    setMessages([...messages, newMessage, botMessage]);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            
+            //setMessages(创);
+        }
+    };
+
+    const handleKeyDown = (event: { key: string; preventDefault: () => void; }) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            handleSendMessage();
+        }
+    };
+
+
+    return (
+        <section className="chat-container">
+            <div className="chatbot-container">
+                <div className="navbar">
+                    <a href="#">ChatNorris</a>
+                </div>
+                <div className="message-container">
+                    {messages.map((msg: Message, i) => (
+                        <div
+                            key={i}
+                            className={`message ${msg.isUser ? 'user' : 'bot'}`}
+                        >
+                            {msg.text}
+                        </div>
+
+                    ))}
+                </div>
+                <div className="dialog">
+                    <input
+                        type="text"
+                        placeholder="Digite sua mensagem..."
+                        value={message}
+                        onChange={handleInputChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <button onClick={handleSendMessage}>Enviar</button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Chat;
